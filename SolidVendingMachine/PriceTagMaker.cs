152bdcf8f5c9ac83571ba9ffdad2c0f714d4914c
sam <evaoosyz@gmail.com>
2022-03-19 15:14:58 +0800

    OCP

--- SolidVendingMachine/PriceTagMaker.cs
+++ SolidVendingMachine/PriceTagMaker.cs
@@ -6,11 +6,30 @@ using System.Threading.Tasks;
 
 namespace SolidVendingMachine
 {
-    internal class PriceTagMaker
+    public class PriceTagMaker
     {
-        public string MakePriceTagForProduct(Product product)
+        public virtual string MakePriceTagForProduct(Product product)
         {
             return $"${product.Price:0.##}";
+        }
+    }
+
+    public class DrinkTypePriceTagMaker : PriceTagMaker
+    {
+        public override string MakePriceTagForProduct(Product product)
+        {
+            var basePrice = base.MakePriceTagForProduct(product);
+            return $"{basePrice} + ${product.Price * 0.1m:0.##} deposit fee";
+        }
+    }
+
+
+    public class FruitTypePriceTagMaker : PriceTagMaker
+    {
+        public override string MakePriceTagForProduct(Product product)
+        {
+            var basePrice = base.MakePriceTagForProduct(product);
+            return $"{basePrice} + ${product.Price * 0.05m:0.##} wrapping fee";
         }
     }
 }
